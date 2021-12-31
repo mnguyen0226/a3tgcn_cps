a7133120b8911a5529f5d18ce0211707519d3885
mnguyen0226 <mnguyen0226@vt.edu>
2021-12-30 20:08:53 -0500

    comment

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -4,6 +4,7 @@ import logging
 
 
 def format_logger(logger, fmt="\033[31m[%(asctime)s %(levelname)s]\033[0m%(message)s"):
+    """Formats the logger messange"""
     handler = logger.handlers[0]
     formatter = logging.Formatter(fmt)
     handler.setFormatter(formatter)
@@ -12,6 +13,7 @@ def format_logger(logger, fmt="\033[31m[%(asctime)s %(levelname)s]\033[0m%(messa
 def output_logger_to_file(
     logger, output_path, fmt="[%(asctime)s %(levelname)s]%(message)s"
 ):
+    """Save the output logger message to a file path"""
     handler = logging.FileHandler(output_path, encoding="UTF-8")
     formatter = logging.Formatter(fmt)
     handler.setFormatter(formatter)
