2a5e0b24b1e0f47e68e38b5fe17e5c95f9f6b65f
mnguyen0226 <mnguyen0226@vt.edu>
2021-12-30 19:47:02 -0500

    add metrics

---
+++ src/utils/metrics.py
@@ -0,0 +1,39 @@
+import torch
+
+# Reference: https://github.com/lehaifeng/T-GCN/tree/master/T-GCN/T-GCN-PyTorch
+
+def accuracy(pred, y):
+    """Calculates the accuracy rate between prediction and actual label
+
+    Args:
+        pred: prediction
+        y: ground-truth label
+
+    Returns:
+        accuracy rate
+    """
+    return 1 - torch.linalg.norm(y - pred, "fro") / torch.linalg.norm(y, "fro")
+
+def r2(pred, y):
+    """Calculates the R1 regression-based metric
+
+    Args:
+        pred: prediction
+        y: ground-truth label
+
+    Returns:
+        r2 rate
+    """
+    return 1 - torch.sum((y - pred) ** 2) / torch.sum((y - torch.mean(pred)) ** 2)
+
+def explained_variance(pred, y):
+    """Calculated variance
+
+    Args:
+        pred: prediction
+        y: ground-truth
+
+    Returns:
+        variance rate
+    """
+    return 1 - torch.var(y - pred) / torch.var(y)
