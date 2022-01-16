9e7b083a010eb056b932602236359f2056f646a4
mnguyen0226 <mnguyen0226@vt.edu>
2022-01-16 12:44:36 -0500

    load

--- learning/save_load_model_tf/load.py
+++ learning/save_load_model_tf/load.py
@@ -6,8 +6,8 @@ def load_model():
     sess=tf.Session()    
     
     #First let's load meta graph and restore weights
-    saver = tf.train.import_meta_graph('trained_models/my_test_model-1000.meta')
-    saver.restore(sess,tf.train.latest_checkpoint('./'))
+    saver = tf.train.import_meta_graph('trained_models/my_test_model.meta')
+    saver.restore(sess,tf.train.latest_checkpoint('./trained_models/'))
     
     
     # Now, let's access and create placeholders variables and
