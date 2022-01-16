169c4081702102cec8d6defabb606c3181b0c54f
mnguyen0226 <mnguyen0226@vt.edu>
2022-01-15 22:57:43 -0500

    commented

--- src/TGCN/tensorflow_model/models/tgcn_cell.py
+++ src/TGCN/tensorflow_model/models/tgcn_cell.py
@@ -6,7 +6,7 @@ from utils import calculate_laplacian
 
 
 class TGCNCell(RNNCell):
-    """Temporal Graph Convolutional Network """
+    """One cell of temporal convolutional neural network architecture including graph convolution and GRU"""
 
     def call(self, inputs, **kwargs):
         pass
@@ -14,6 +14,16 @@ class TGCNCell(RNNCell):
     def __init__(
         self, num_units, adj, num_nodes, input_size=None, act=tf.nn.tanh, reuse=None
     ):
+        """Constructors of one TGCN Cell.
+
+        Args:
+            num_units: Number of units.
+            adj: Adjacency matrix.
+            num_nodes: Number of nodes
+            input_size: Input size. Defaults to None.
+            act: Logistic regression function for supervised learning. Defaults to tf.nn.tanh.
+            reuse: Defaults to None.
+        """
 
         super(TGCNCell, self).__init__(_reuse=reuse)
         self._act = act
@@ -22,31 +32,60 @@ class TGCNCell(RNNCell):
         self._adj = []
         self._adj.append(calculate_laplacian(adj))
 
-    @property
+    @property # getter and setter in python class
     def state_size(self):
         return self._nodes * self._units
 
-    @property
+    @property # getter and setter in python class
     def output_size(self):
         return self._units
 
     def __call__(self, inputs, state, scope=None):
+        """Built-in methods in Python enable to write classes where the instances behave like function and can be called as function.
+        Reference: https://www.geeksforgeeks.org/__call__-in-python/
+        
+        These are functions in 1 TGCN cells: https://arxiv.org/pdf/1811.05320.pdf
 
+        Args:
+            inputs: Inputs.
+            state: State.
+            scope: Defaults to None.
+
+        Returns:
+            [type]: [description]
+        """
         with tf.variable_scope(scope or "tgcn"):
-            with tf.variable_scope("gates"):
+            with tf.variable_scope("gates"):    
+                # ut = σ(Wu [f(A, Xt), ht−1] + bu)
+                # rt = σ(Wr [f(A, Xt), ht−1] + br) 
                 value = tf.nn.sigmoid(
                     self._gc(inputs, state, 2 * self._units, bias=1.0, scope=scope)
                 )
                 r, u = tf.split(value=value, num_or_size_splits=2, axis=1)
+                
             with tf.variable_scope("candidate"):
+                # ct = tanh(Wc [f(A, Xt),(rt ∗ ht−1)] + bc)
                 r_state = r * state
                 c = self._act(self._gc(inputs, r_state, self._units, scope=scope))
+                
+            # ht = ut ∗ ht−1 + (1 − ut) ∗ ct
             new_h = u * state + (1 - u) * c
+            
         return new_h, new_h
 
     def _gc(
         self, inputs, state, output_size, bias=0.0, scope=None
-    ):  # graph convolutional networks
+    ):  
+        """Graph Convolution cell.
+        
+        Args:
+            inputs: Inputs.
+            state: States.
+            output_size: Output size.
+            bias: Bias rate. Defaults to 0.0
+            scope: Defaults to None.
+        
+        """
         ## inputs:(-1,num_nodes)
         inputs = tf.expand_dims(inputs, 2)
 
