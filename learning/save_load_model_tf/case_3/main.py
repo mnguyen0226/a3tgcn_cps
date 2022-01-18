23947a7c24a51ea1665a6321a9ac4750ec66c486
mnguyen0226 <mnguyen0226@vt.edu>
2022-01-18 10:13:11 -0500

    trained

--- learning/save_load_model_tf/case_3/main.py
+++ learning/save_load_model_tf/case_3/main.py
@@ -112,7 +112,7 @@ def load_and_retrain(): # expect the accuracy to increase
         
         # Restore model weights from previously saved model
         load_path = saver.restore(sess, model_path)
-        print("Model restored from file: %s" % model_path)
+        # print("Model restored from file: %s" % model_path)
 
         # Resume training
         for epoch in range(7):
@@ -154,7 +154,7 @@ def load_and_eval():
         
         # Restore model weights from previously saved model
         load_path = saver.restore(sess, model_path)
-        print("Model restored from file: %s" % model_path)
+        # print("Model restored from file: %s" % model_path)
 
         # Test model
         correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
@@ -165,9 +165,9 @@ def load_and_eval():
 
 # User Interface
 def main():
-    # train_and_eval() 
+    train_and_eval() 
     # load_and_retrain() # and save model
-    load_and_eval()
+    # load_and_eval()
     
 if __name__ == "__main__":
     main()
