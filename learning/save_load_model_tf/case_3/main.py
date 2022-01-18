b1a2aa86bd4c9910db71a42af0c9e4c6c6fff505
mnguyen0226 <mnguyen0226@vt.edu>
2022-01-18 09:44:37 -0500

    train a model

--- learning/save_load_model_tf/case_3/main.py
+++ learning/save_load_model_tf/case_3/main.py
@@ -1,5 +1,5 @@
 # Reference: https://github.com/aymericdamien/TensorFlow-Examples/blob/master/tensorflow_v1/notebooks/4_Utils/save_restore_model.ipynb
-# File trains simple neural network in MNIST and saves model locally.
+# File trains simple neural network in MNIST, saves, and loads model locally.
 
 from pickletools import optimize
 import tensorflow as tf
@@ -11,7 +11,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
 # Sets parameters
 learning_rate = 0.001
 batch_size = 100
-display_step = 4
+display_step = 1
 model_path = "trained_model/model.ckpt"
 
 # Network parameters
@@ -64,7 +64,9 @@ init = tf.global_variables_initializer()
 # 'Saver" op to save and restore all the variables
 saver = tf.train.Saver()
             
-def train():
+global_save_path = ""  
+            
+def train_and_eval():
     # Run first session
     print("Start 1st session...")
     with tf.Session() as sess:
@@ -101,7 +103,60 @@ def train():
         
         # Save model weights to disk
         save_path = saver.save(sess, model_path)
+        global_save_path = save_path
         print("Model saved in file: %s" % save_path)
+
+def load_and_retrain(): # expect the accuracy to increase
+    print("\nLoad model and continue to train")
+    with tf.Session() as sess:
+        # Initialize variables
+        sess.run(init)
+        
+        # Restore model weights from previously saved model
+        load_path = saver.restore(sess, model_path)
+        print("Model restored from file: %s" % global_save_path)
+
+        # Resume training
+        for epoch in range(7):
+            avg_loss = 0.0
+            total_batch = int(mnist.train.num_examples / batch_size)
+            
+            # Loops over all batches
+            for i in range(total_batch):
+                batch_x, batch_y = mnist.train.next_batch(batch_size)
+                
+                # Run optimization op (backprop) and cost op (to get loss value)
+                _, c = sess.run([optimizer, loss], feed_dict = {x: batch_x, y: batch_y})
+                
+                # Compute average loss
+                avg_loss += c / total_batch
+            
+            # Display logs per epoch step
+            if(epoch % display_step == 0):
+                print("Epoch: ", '%04d' % (epoch+1), "; Loss: ", "{:.9f}".format(avg_loss))
+        print("Second Optimization Finished!")
+        
+        # Test model
+        correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        
+        # Calculate accuracy
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        print("Accuracy: ", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+        
+        # # Save model weights to disk
+        # save_path = saver.save(sess, model_path)
+        # global_save_path = save_path
+        # print("Model saved in file: %s" % save_path)
+        
+        
+def load_and_eval():
+    print("Load model and evaluate")
+
+# User Interface
+def main():
+    train_and_eval() 
+    
+    # load_and_retrain()
     
 if __name__ == "__main__":
-    train()
+    main()
