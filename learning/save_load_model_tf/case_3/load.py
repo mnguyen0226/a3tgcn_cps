d08eb66a63795ce13974fe3246757baa53536116
mnguyen0226 <mnguyen0226@vt.edu>
2022-01-18 09:07:17 -0500

    minor change

---
+++ learning/save_load_model_tf/case_3/load.py
@@ -0,0 +1,2 @@
+# Reference: https://github.com/aymericdamien/TensorFlow-Examples/blob/master/tensorflow_v1/notebooks/4_Utils/save_restore_model.ipynb
+
--- learning/save_load_model_tf/case_3/train.py
+++ learning/save_load_model_tf/case_3/train.py
@@ -1,4 +1,5 @@
 # Reference: https://github.com/aymericdamien/TensorFlow-Examples/blob/master/tensorflow_v1/notebooks/4_Utils/save_restore_model.ipynb
+# File trained simple n
 
 from pickletools import optimize
 import tensorflow as tf
