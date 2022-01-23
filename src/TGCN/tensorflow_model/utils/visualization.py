bac02f8cfaaf5509ad7aab227a0bf4d8ee1e8cfe
Minh T. Nguyen <mnguyen0226@pop-os.localdomain>
2022-01-23 14:58:58 -0500

    change to linux-supported image

--- src/TGCN/tensorflow_model/utils/visualization.py
+++ src/TGCN/tensorflow_model/utils/visualization.py
@@ -20,7 +20,7 @@ def plot_result(test_result, test_label, path):
     plt.plot(a_pred, "r-", label="prediction")
     plt.plot(a_true, "b-", label="true")
     plt.legend(loc="best", fontsize=10)
-    plt.savefig(path + "/test_all.jpg")
+    plt.savefig(path + "/test_all.png")
     plt.show()
 
     # oneday test result visualization
@@ -32,7 +32,7 @@ def plot_result(test_result, test_label, path):
     plt.plot(a_pred, "r-", label="prediction")
     plt.plot(a_true, "b-", label="true")
     plt.legend(loc="best", fontsize=10)
-    plt.savefig(path + "/test_oneday.jpg")
+    plt.savefig(path + "/test_oneday.png")
     plt.show()
 
 
@@ -53,60 +53,60 @@ def plot_error(train_rmse, train_loss, test_rmse, test_acc, test_mae, path, plot
         plt.plot(train_rmse, "r-", label="train_rmse")
         plt.plot(test_rmse, "b-", label="test_rmse")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/rmse.jpg")
+        plt.savefig(path + "/rmse.png")
         plt.show()
 
         # train_loss & train_rmse
         fig1 = plt.figure(figsize=(5, 3))
         plt.plot(train_loss, "b-", label="train_loss")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/train_loss.jpg")
+        plt.savefig(path + "/train_loss.png")
         plt.show()
 
         fig1 = plt.figure(figsize=(5, 3))
         plt.plot(train_rmse, "b-", label="train_rmse")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/train_rmse.jpg")
+        plt.savefig(path + "/train_rmse.png")
         plt.show()
 
         # test accuracy
         fig1 = plt.figure(figsize=(5, 3))
         plt.plot(test_acc, "b-", label="test_acc")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/test_acc.jpg")
+        plt.savefig(path + "/test_acc.png")
         plt.show()
 
         # test rmse
         fig1 = plt.figure(figsize=(5, 3))
         plt.plot(test_rmse, "b-", label="test_rmse")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/test_rmse.jpg")
+        plt.savefig(path + "/test_rmse.png")
         plt.show()
 
         # test mae
         fig1 = plt.figure(figsize=(5, 3))
         plt.plot(test_mae, "b-", label="test_mae")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/test_mae.jpg")
+        plt.savefig(path + "/test_mae.png")
         plt.show()
     elif(plot_eval == True):
         # train accuracy
         fig1 = plt.figure(figsize=(5, 3))
         plt.plot(test_acc, "b-", label="test_acc")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/test_acc.jpg")
+        plt.savefig(path + "/test_acc.png")
         plt.show()
 
         # train rmse
         fig1 = plt.figure(figsize=(5, 3))
         plt.plot(test_rmse, "b-", label="test_rmse")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/test_rmse.jpg")
+        plt.savefig(path + "/test_rmse.png")
         plt.show()
 
         # train mae
         fig1 = plt.figure(figsize=(5, 3))
         plt.plot(test_mae, "b-", label="test_mae")
         plt.legend(loc="best", fontsize=10)
-        plt.savefig(path + "/test_mae.jpg")
+        plt.savefig(path + "/test_mae.png")
         plt.show()
