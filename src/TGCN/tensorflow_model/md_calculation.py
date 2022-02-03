0282998f778679fd0cf22f373fdf2312fad287f7
mnguyen0226 <mnguyen0226@vt.edu>
2022-02-03 16:46:50 -0500

    start md calculation script

---
+++ src/TGCN/tensorflow_model/md_calculation.py
@@ -0,0 +1,15 @@
+# Scripts takes in the labels and prediction dataset to calculate the mahalanobis distance
+# Test calculate the first 10 rows
+# Test calculate the first 20 rows
+# Calculate and return if there is any negative numbers
+
+
+import pandas as pd
+import numpy as np
+from scipy.stats import chi2
+
+def calculate_md():
+    print("Calculate Mahalanobis Distance")
+    
+if __name__ == "__main__":
+    calculate_md()
