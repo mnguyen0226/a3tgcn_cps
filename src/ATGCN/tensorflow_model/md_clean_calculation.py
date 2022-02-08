7dfe113e2b5d2eb6d9731134c1729d561907bf2d
mnguyen0226 <mnguyen0226@vt.edu>
2022-02-07 22:57:12 -0500

    add robust md

--- src/ATGCN/tensorflow_model/md_clean_calculation.py
+++ src/ATGCN/tensorflow_model/md_clean_calculation.py
@@ -12,44 +12,85 @@ from pandas import cut
 from scipy.stats import chi2
 import csv
 import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy as sp
+from sklearn.covariance import MinCovDet
 
 EVAL_CLEAN_LABEL_DIR = "out/tgcn/tgcn_scada_wds_lr0.005_batch128_unit64_seq8_pre1_epoch101/eval_clean/eval_clean_labels.csv"
 EVAL_CLEAN_PREDS_DIR = "out/tgcn/tgcn_scada_wds_lr0.005_batch128_unit64_seq8_pre1_epoch101/eval_clean/eval_clean_output.csv"
-L = 25
-TH = 43.5
-GLOBAL_ME = np.array(  # Recall calculate everytime retrained model
+### Poisoned Dataset
+# L = 30 #25
+# UPPER_TH = 40.5
+
+L = 30 #25
+UPPER_TH = 40.5
+LOWER_TH = 16
+GLOBAL_MEAN_ERROR = np.array(  # Recall calculate everytime retrained model
+    # [
+    #     0.470392,
+    #     0.46830426,
+    #     1.33373831,
+    #     0.79339774,
+    #     1.50138811,
+    #     0.42727061,
+    #     -0.09867107,
+    #     1.13456807,
+    #     -1.11697018,
+    #     0.3630304,
+    #     -1.00766091,
+    #     0.62395863,
+    #     -0.27923139,
+    #     -0.50331974,
+    #     -1.27372116,
+    #     0.72420398,
+    #     -0.25789348,
+    #     0.85660608,
+    #     0.15255812,
+    #     0.21225869,
+    #     -0.98057355,
+    #     0.8983543,
+    #     0.89394346,
+    #     0.6814819,
+    #     0.13756092,
+    #     1.47610716,
+    #     0.54627279,
+    #     1.62522872,
+    #     -0.87679475,
+    #     0.47024836,
+    #     2.40034568,
+    # ]
     [
-        0.470392,
-        0.46830426,
-        1.33373831,
-        0.79339774,
-        1.50138811,
-        0.42727061,
-        -0.09867107,
-        1.13456807,
-        -1.11697018,
-        0.3630304,
-        -1.00766091,
-        0.62395863,
-        -0.27923139,
-        -0.50331974,
-        -1.27372116,
-        0.72420398,
-        -0.25789348,
-        0.85660608,
-        0.15255812,
-        0.21225869,
-        -0.98057355,
-        0.8983543,
-        0.89394346,
-        0.6814819,
-        0.13756092,
-        1.47610716,
-        0.54627279,
-        1.62522872,
-        -0.87679475,
-        0.47024836,
-        2.40034568,
+        0.52865503,
+        0.45958133,
+        1.25685963,
+        0.77171607,
+        1.48401157,
+        0.47557998,
+        -0.10165968,
+        1.2389529,
+        -2.20679925,
+        0.3992842,
+        -1.20916699,
+        0.59666763,
+        -0.30926249,
+        -0.48957757,
+        -1.22333392,
+        0.78566197,
+        -0.17307536,
+        0.88205641,
+        0.08203033,
+        0.32419844,
+        -1.13448625,
+        0.99462164,
+        0.7230496,
+        0.73255892,
+        0.2656382,
+        1.4046767,
+        0.48527626,
+        1.5610318,
+        -0.81215967,
+        0.46683175,
+        2.43063466,
     ]
 )
 
@@ -126,6 +167,8 @@ def calculate_md_clean():
 
     # 3. Calculate the global mean error arrayy
     global_mean_error = np.mean(df_error, axis=0)
+
+    # Save the global mean error
     f = open(
         "out/tgcn/tgcn_scada_wds_lr0.005_batch128_unit64_seq8_pre1_epoch101/eval_clean/global_mean_error_clean.txt",
         "w",
@@ -136,7 +179,7 @@ def calculate_md_clean():
     # 4. Calculate the mahalanobis distance
     distances = []
     for i, val in enumerate(df_error):
-        p1 = val  # Ozone and Temp of the ith row
+        p1 = val
         p2 = global_mean_error
         distance = (
             (p1 - p2).T.dot(covariance_pm1).dot(p1 - p2)
@@ -154,11 +197,11 @@ def calculate_md_clean():
         cutoff_arr.append(mean_batch_squared_md)
     print(len(cutoff_arr))
     print(f"The Average Mean Squared Mahalanobis Distance {np.average(cutoff_arr)}")
-    
-    threshold_line_clean = [TH for x in range(len(cutoff_arr))] 
 
-    plt.plot(cutoff_arr, label = "mean batch squared md")
-    plt.plot(threshold_line_clean, label = "attacks threshold")
+    threshold_line_clean = [UPPER_TH for x in range(len(cutoff_arr))]
+
+    plt.plot(cutoff_arr, label="mean batch squared md")
+    plt.plot(threshold_line_clean, label="attacks threshold")
     plt.title(
         "Mean Squared Mahalanobis Distance Every L Hours TimeStamp - Clean Dataset"
     )
@@ -191,5 +234,80 @@ def calculate_md_clean():
     # # print(df_error[ distances > cutoff , :])
 
 
+##########
+def calculate_rmd_clean():
+    """Calculates Robust Mahalanobus Distance clean dataset"""
+    df_eval_labels, df_eval_preds = data_preprocessing(num_line=1744)
+
+    # Convert lists to numpy arrays
+    df_eval_labels = np.array(df_eval_labels)
+    df_eval_preds = np.array(df_eval_preds)
+
+    # Get error array between all labels and all predictions
+    df_error = df_eval_labels - df_eval_preds
+
+    # 1. Calculate Minimum Covariance Determinant
+    rng = np.random.RandomState(0)
+
+    # 2. Calculate the covariance matrix
+    real_cov = np.cov(df_error, rowvar=False)
+
+    # 3. Get multivariate values
+    X = rng.multivariate_normal(mean=np.mean(df_error, axis=0), cov=real_cov, size=506)
+
+    # 4. Get MCD values
+    cov = MinCovDet(random_state=0).fit(X)
+    mcd = cov.covariance_  # robust covariance metrics
+
+    # 5. Calculate the global mean error
+    global_mean_error = cov.location_
+
+    # Save the global mean error
+    f = open(
+        "out/tgcn/tgcn_scada_wds_lr0.005_batch128_unit64_seq8_pre1_epoch101/eval_clean/global_mean_error_clean.txt",
+        "a",
+    )
+    f.write("\n\n")
+    f.write(np.array2string(global_mean_error, separator=","))
+    f.close()
+
+    # 6. Calculate the invert covariance matrix
+    inv_covmat = sp.linalg.inv(mcd)
+
+    # 4. Calculate the mahalanobis distance
+    distances = []
+    for i, val in enumerate(df_error):
+        p1 = val
+        p2 = global_mean_error
+        distance = (
+            (p1 - p2).T.dot(inv_covmat).dot(p1 - p2)
+        )  # squared mahalanobis distance
+        distances.append(distance)
+        # print(f"Distance: {distance}")
+    distances = np.array(distances)  # 1744 values
+
+    cutoff_arr = []
+
+    for i in range(L, (len(distances))):
+        batch_squared_md = distances[i - L : i]  # take the first L batches
+        mean_batch_squared_md = np.average(batch_squared_md)
+        # batch_cutoff = chi2.pff(0.95, 31)
+        cutoff_arr.append(mean_batch_squared_md)
+    print(len(cutoff_arr))
+    print(f"The Average Mean Squared Mahalanobis Distance {np.average(cutoff_arr)}")
+
+    threshold_line_clean = [UPPER_TH for x in range(len(cutoff_arr))]
+
+    plt.plot(cutoff_arr, label="mean batch squared md")
+    plt.plot(threshold_line_clean, label="attacks threshold")
+    plt.title(
+        "Mean Squared Mahalanobis Distance Every L Hours TimeStamp - Clean Dataset"
+    )
+    plt.xlabel("Every L hours")
+    plt.ylabel("Mean Squared Mahalanobis Distance - To Calibrate Max Threshold")
+    plt.show()
+
+
 if __name__ == "__main__":
     calculate_md_clean()
+    calculate_rmd_clean()
--- src/ATGCN/tensorflow_model/md_poison_calculation.py
+++ src/ATGCN/tensorflow_model/md_poison_calculation.py
@@ -4,24 +4,28 @@ from scipy.stats import chi2
 import csv
 from md_clean_calculation import data_preprocessing
 import matplotlib.pyplot as plt
-from md_clean_calculation import GLOBAL_ME
+from md_clean_calculation import GLOBAL_MEAN_ERROR
 from md_clean_calculation import L
-from md_clean_calculation import TH
+from md_clean_calculation import UPPER_TH
+from md_clean_calculation import LOWER_TH
 import pandas as pd
+import seaborn as sns
+import scipy as sp
+from sklearn.covariance import MinCovDet
 
 # Before any attacks there will be a 17 hour time stamps
 EVAL_POISON_LABEL_DIR = "out/tgcn/tgcn_scada_wds_lr0.005_batch128_unit64_seq8_pre1_epoch101/eval_poisoned/eval_poisoned_labels.csv"
 EVAL_POISON_PREDS_DIR = "out/tgcn/tgcn_scada_wds_lr0.005_batch128_unit64_seq8_pre1_epoch101/eval_poisoned/eval_poisoned_output.csv"
-EVAL_POISON_LINE_NUM = 4168 # CHANGE for each different eval_poisoned_output.csv
+EVAL_POISON_LINE_NUM = 4168  # CHANGE for each different eval_poisoned_output.csv
 
-dataset04 = pd.read_csv(r"data/processed/processed_dataset04_origin_binary.csv") # CHANGE for each different poisoned dataset.csv
+dataset04 = pd.read_csv(
+    r"data/processed/processed_dataset04_origin_binary.csv"
+)  # CHANGE for each different poisoned dataset.csv
 
 binary_arr = dataset04["ATT_FLAG"].to_list()
 binary_arr = binary_arr[(L + 8) : -1]  # use 8 for prediction + L for first window size
-print(len(binary_arr))
-
-convert_th_binary_arr = [20 if x == 0 else TH for x in binary_arr]
-print(len(convert_th_binary_arr))
+# 
+convert_th_binary_arr = [LOWER_TH if x == 0 else UPPER_TH for x in binary_arr]
 
 ##########
 def calculate_md_poison():
@@ -47,7 +51,7 @@ def calculate_md_poison():
     covariance_pm1 = np.linalg.matrix_power(cov, -1)
 
     # 3. Calculate the mean error arrayy
-    global_mean_error = GLOBAL_ME
+    global_mean_error = GLOBAL_MEAN_ERROR
 
     # 4. Calculate the mahalanobis distance
     distances = []
@@ -69,8 +73,8 @@ def calculate_md_poison():
     print(len(cutoff_arr))
     print(f"The Average Mean Squared Mahalanobis Distance {np.average(cutoff_arr)}")
 
-    plt.plot(cutoff_arr, label = "mean batch squared md")
-    plt.plot(convert_th_binary_arr, label = "attacks threshold")
+    plt.plot(cutoff_arr, label="mean batch squared md")
+    plt.plot(convert_th_binary_arr, label="attacks threshold")
     plt.title(
         "Mean Squared Mahalanobis Distance Every L Hours TimeStamp - Poisoned Dataset"
     )
@@ -104,5 +108,81 @@ def calculate_md_poison():
     # # print(df_error[ distances > cutoff , :])
 
 
+##########
+def calculate_rmd_poison():
+    """Calculates the Mahalanobis Distance for poisoned dataset"""
+    # Get lists
+    df_eval_labels, df_eval_preds = data_preprocessing(
+        num_line=EVAL_POISON_LINE_NUM,
+        label_dataset=EVAL_POISON_LABEL_DIR,
+        preds_dataset=EVAL_POISON_PREDS_DIR,
+    )
+
+    # Convert lists to numpy arrays
+    df_eval_labels = np.array(df_eval_labels)
+    df_eval_preds = np.array(df_eval_preds)
+
+    # Get error array
+    df_error = df_eval_labels - df_eval_preds
+
+    # 1. Calculate Minimum Covariance Determinant
+    rng = np.random.RandomState(0)
+
+    # 2. Calculate the covariance matrix
+    real_cov = np.cov(df_error, rowvar=False)
+
+    # 3. Get multivariate values
+    X = rng.multivariate_normal(mean=np.mean(df_error, axis=0), cov=real_cov, size=506)
+
+    # 4. Get MCD values
+    cov = MinCovDet(random_state=0).fit(X)
+    mcd = cov.covariance_  # robust covariance metrics
+
+    # 5. Calculate the invert covariance matrix
+    inv_covmat = sp.linalg.inv(mcd)
+
+    # 6. Calculate the mahalanobis distance
+    distances = []
+    for i, val in enumerate(df_error):
+        p1 = val  # Ozone and Temp of the ith row
+        p2 = GLOBAL_MEAN_ERROR
+        distance = (p1 - p2).T.dot(inv_covmat).dot(p1 - p2)
+        distances.append(distance)
+        # print(f"Distance: {distance}")
+    distances = np.array(distances)
+
+    mean_batch_squared_md_arr = []
+
+    outliers = []
+
+    for i in range(L, (len(distances))):
+        batch_squared_md = distances[i - L : i]  # take the first L batches
+        mean_batch_squared_md = np.average(batch_squared_md)
+        mean_batch_squared_md_arr.append(mean_batch_squared_md)
+        if(mean_batch_squared_md >= UPPER_TH):
+            outliers.append(UPPER_TH)
+        else:
+            outliers.append(LOWER_TH)
+        
+    print(f"The Average Mean Squared Mahalanobis Distance {np.average(mean_batch_squared_md_arr)}")
+
+    fig1 = plt.figure(figsize=(5, 3))
+    plt.plot(mean_batch_squared_md_arr, label="mean squared batch squared md")
+    plt.plot(convert_th_binary_arr, label="attacks labels")
+    plt.title(
+        "Mean Squared Robust Mahalanobis Distance Every L Hours TimeStamp - Poisoned Dataset"
+    )
+    plt.xlabel("Every L hours")
+    plt.ylabel("Mean Squared Robust Mahalanobis Distance - To Calibrate Max Threshold")
+    plt.legend()
+    plt.show()
+    
+    fig1 = plt.figure(figsize=(5, 3))
+    plt.plot(convert_th_binary_arr, label="attacks labels")
+    plt.plot(outliers, label="attacks predictions")
+    plt.legend()
+    plt.show()
+
 if __name__ == "__main__":
-    calculate_md_poison()
+    # calculate_md_poison()
+    calculate_rmd_poison()
